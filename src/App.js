fcee82b15ef2d0fcbaa6cf377e9e34819c7fd734
emmanuel-storm <61197043+emmanuel-storm@users.noreply.github.com>
2021-03-19 17:51:43 +0100

    j'ai affiché les taches en liste et j'ai rajouté une zone de texte pour rajouter une tache   /   la tache se rajoute

--- src/App.js
+++ src/App.js
@@ -4,7 +4,34 @@ import "./App.css";
 const fetchURL = "https://6053736845e4b30017291b83.mockapi.io/tasks";
 const getItems = () => fetch(fetchURL).then(res => res.json());
 
+function TaskForm({ addTask }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTask(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="addTask"
+        value={value}
+        placeholder="add a task"
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
 function App() {
+  const addTask = description => {
+    const newItems = [...items, { description }];
+    setItems(newItems);
+  };
   const [items, setItems] = useState([]);
 
   useEffect(() => {
@@ -13,11 +40,15 @@ function App() {
   console.log(items);
   return (
     <div className="app">
-      <div className="todo">
+      <TaskForm addTask={addTask} />
+      <h1>CURRENT TASKS</h1>
+      <ul className="todo-list">
         {items.map(item => (
-          <div key={item.id}>{item.description}</div>
+          <li className="todo" key={item.id}>
+            <p>{item.description}</p>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 }
