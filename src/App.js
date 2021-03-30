22593df2c3f5bf36c511226bee60e0cc13cf2173
emmanuel-storm <61197043+emmanuel-storm@users.noreply.github.com>
2021-03-31 00:38:51 +0200

    refactoring du code et debut de piste pour la suppression et la mise à jour d'une tache

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import CurrentTasks from "./CurrentTasks";
-import TaskFom from "./TaskForm";
 
 function App() {
   return (
     <>
       <CurrentTasks />
-      <TaskFom />
     </>
   );
 }
--- src/CurrentTasks.js
+++ src/CurrentTasks.js
@@ -1,15 +1,91 @@
 import React, { useState, useEffect } from "react";
-import TaskForm from "./TaskForm";
 import format from "date-fns/format";
+import { Button, Card, Form } from "react-bootstrap";
+//pour la date
+import moment from "moment";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+function Todo({ item, index, markTask, removeTask }) {
+  return (
+    <div className="todo">
+      <span style={{ textDecoration: item.isComplete ? "line-through" : "" }}>
+        {" "}
+      </span>
+      <div>
+        <Button variant="outline-success" onClick={() => markTask(index)}>
+          {" "}
+          ✓{" "}
+        </Button>{" "}
+        {""}
+        <Button variant="outline-danger" onClick={() => removeTask(index)}>
+          {" "}
+          ✕{" "}
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+function TaskForm({ addTask }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTask(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label>
+          {" "}
+          <b>Add task</b>{" "}
+        </Form.Label>
+        <Form.Control
+          type="text"
+          className="input"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          placeholder="add new task"
+        />
+      </Form.Group>
+      <Button variant="primary mb-3" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
 
 function CurrentTasks() {
+  // ajouter une tache
+
+  const addTask = (description, createdAt) => {
+    const newItems = [{ description, createdAt }, ...items];
+    createdAt = "22 / 03 / 2001 - 13:43:45";
+    setItems(newItems);
+  };
+
+  // marquer une tache comme complete ou !
+
+  const markTask = index => {
+    const newItems = [...items];
+    newItems[index].isComplete = true;
+    setItems(newItems);
+  };
+
+  // supprimer une tache
+
+  const removeTask = index => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+  };
+
   const fetchURL = "https://6053736845e4b30017291b83.mockapi.io/tasks";
   const getItems = () => fetch(fetchURL).then(res => res.json());
 
-  const addTask = description => {
-    const newItems = [{ description }, ...items];
-    setItems(newItems);
-  };
   const [items, setItems] = useState([]);
 
   useEffect(() => {
@@ -25,17 +101,24 @@ function CurrentTasks() {
 
   return (
     <div className="app">
-      <TaskForm addTask={addTask} />
       <h1>CURRENT TASKS</h1>
       <ul className="todo-list">
+        <TaskForm addTask={addTask} />
         {items
           .filter(item => item.isComplete === false)
           .map(item => (
             <li className="todo" key={item.id}>
               <p>
-                {format(new Date(item.createdAt), "dd/MM/yyyy kk:mm")} -{" "}
+                {format(new Date(item.createdAt), "dd/MM/yyyy kk:mm:ss")} -{" "}
                 {item.description}
               </p>
+              <Todo
+                key={item}
+                item={item}
+                // todo={todo}
+                markTask={markTask}
+                removeTask={removeTask}
+              />
             </li>
           ))}
       </ul>
